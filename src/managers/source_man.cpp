cbb60b7912267bf3f68c3d18a1efeab837e47e87
Dr. Michael DeCicco <mdecicco8@gmail.com>
2020-04-08 22:12:22 -0400

    added managers directory, source_man.h/cpp

---
+++ src/managers/source_man.cpp
@@ -0,0 +1,11 @@
+#include <managers/source_man.h>
+#include <utils/audio.h>
+#include <utils/video.h>
+
+namespace rosen {
+	source_man::source_man() {
+	}
+	
+	source_man::~source_man() {
+	}
+};
---
+++ src/managers/source_man.h
@@ -0,0 +1,10 @@
+namespace rosen {
+	class video_container;
+	class audio_container;
+
+	class source_man {
+		public:
+			source_man();
+			~source_man();
+	};
+};
